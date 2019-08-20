6361e0c2f024f5506282a45b9754fb1e1dbdb042
Benjamin Laird <benjamin.laird@acmi.net.au>
2019-08-20 17:09:56 +1000

    Rollback script to subprocess call

--- recipes/script.py
+++ recipes/script.py
@@ -1,43 +1,31 @@
-# Copyright (c) 2014 Museum Victoria
-# Modifications copyright (c) 2019 ACMI
+# Copyright (c) 2014Museum Victoria
 # This software is released under the MIT license (see license.txt for details)
 
 """This node provides raspberry pi controls."""
 
-import os
-import urllib2
-
-BALENA_SUPERVISOR_ADDRESS = os.getenv('BALENA_SUPERVISOR_ADDRESS')
-BALENA_SUPERVISOR_API_KEY = os.getenv('BALENA_SUPERVISOR_API_KEY')
+import subprocess
 
 def shutdown():
-    url = BALENA_SUPERVISOR_ADDRESS + '/v1/shutdown?apikey=' + BALENA_SUPERVISOR_API_KEY
-    print url
-    request = urllib2.Request(url)
-    request.add_header('Content-Type', 'application/json')
-    response = urllib2.urlopen(request)
+  returncode = subprocess.call('shutdown -h now', shell=True)
+
 
 def reboot():
-    url = BALENA_SUPERVISOR_ADDRESS + '/v1/reboot?apikey=' + BALENA_SUPERVISOR_API_KEY
-    print url
-    request = urllib2.Request(url)
-    request.add_header('Content-Type', 'application/json')
-    response = urllib2.urlopen(request)
+  returncode = subprocess.call('reboot now', shell=True)
 
 
 # Local actions this Node provides
 def local_action_TurnOff(arg=None):
-    """{"title":"Turn off","desc":"Turns this computer off.","group":"Power"}"""
-    print 'Action TurnOff requested'
-    shutdown()
+  """{"title":"Turn off","desc":"Turns this computer off.","group":"Power"}"""
+  print 'Action TurnOff requested'
+  shutdown()
 
 
 def local_action_Reboot(arg=None):
-    """{"title":"Reboot","desc":"Turns this computer off.","group":"Power","caution":"Ensure hardware is in a state to be rebooted."}"""
-    print 'Action Reboot requested'
-    reboot()
+  """{"title":"Reboot","desc":"Turns this computer off.","group":"Power","caution":"Ensure hardware is in a state to be rebooted."}"""
+  print 'Action Reboot requested'
+  reboot()
 
 
 def main(arg=None):
-    # Start your script here.
-    print 'Nodel script started.'
+  # Start your script here.
+  print 'Nodel script started.'
