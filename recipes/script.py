a133e230eee43f21d9ae996ffd74aa8a2a37caaa
Benjamin Laird <benjamin.laird@acmi.net.au>
2019-08-20 17:11:50 +1000

    Set indentation to 4 spaces

--- recipes/script.py
+++ recipes/script.py
@@ -6,26 +6,26 @@
 import subprocess
 
 def shutdown():
-  returncode = subprocess.call('shutdown -h now', shell=True)
+    returncode = subprocess.call('shutdown -h now', shell=True)
 
 
 def reboot():
-  returncode = subprocess.call('reboot now', shell=True)
+    returncode = subprocess.call('reboot now', shell=True)
 
 
 # Local actions this Node provides
 def local_action_TurnOff(arg=None):
-  """{"title":"Turn off","desc":"Turns this computer off.","group":"Power"}"""
-  print 'Action TurnOff requested'
-  shutdown()
+    """{"title":"Turn off","desc":"Turns this computer off.","group":"Power"}"""
+    print 'Action TurnOff requested'
+    shutdown()
 
 
 def local_action_Reboot(arg=None):
-  """{"title":"Reboot","desc":"Turns this computer off.","group":"Power","caution":"Ensure hardware is in a state to be rebooted."}"""
-  print 'Action Reboot requested'
-  reboot()
+    """{"title":"Reboot","desc":"Turns this computer off.","group":"Power","caution":"Ensure hardware is in a state to be rebooted."}"""
+    print 'Action Reboot requested'
+    reboot()
 
 
 def main(arg=None):
-  # Start your script here.
-  print 'Nodel script started.'
+    # Start your script here.
+    print 'Nodel script started.'
