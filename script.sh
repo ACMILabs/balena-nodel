577c024c13d2aae224c09ec102a70a3456cf9d3b
Benjamin Laird <benjamin.laird@acmi.net.au>
2019-08-21 16:27:05 +1000

    Try setting interface with IP address

--- script.sh
+++ script.sh
@@ -1,8 +1,6 @@
 # Configure virtual network interface for Nodel
 IP_ADDRESS=$(curl -X GET --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v1/device?apikey=$BALENA_SUPERVISOR_API_KEY" | jq -r .ip_address)
-ip link add nodel0 type dummy
-ifconfig nodel0 $IP_ADDRESS
 mkdir -p /opt/nodel/nodes/$BALENA_DEVICE_NAME_AT_INIT
 cp -f /opt/nodel/scripts/script.py /opt/nodel/nodes/$BALENA_DEVICE_NAME_AT_INIT/
 # Start Nodel
-java -jar /opt/nodel/nodel.jar --interface nodel0
+java -jar /opt/nodel/nodel.jar --interface $IP_ADDRESS
