7876e8b91ef1e75fa9c6632ace706afc430981c9
Benjamin Laird <benjamin.laird@acmi.net.au>
2019-08-21 16:30:54 +1000

    Remove IP address and restore Nodel interface environment variable

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
-# Configure virtual network interface for Nodel
-IP_ADDRESS=$(curl -X GET --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v1/device?apikey=$BALENA_SUPERVISOR_API_KEY" | jq -r .ip_address)
+# Copy recipe to directory based on device name
 mkdir -p /opt/nodel/nodes/$BALENA_DEVICE_NAME_AT_INIT
 cp -f /opt/nodel/scripts/script.py /opt/nodel/nodes/$BALENA_DEVICE_NAME_AT_INIT/
 # Start Nodel
-java -jar /opt/nodel/nodel.jar --interface $IP_ADDRESS
+java -jar /opt/nodel/nodel.jar --interface $NODEL_INTERFACE
