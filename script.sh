5f1bb0709483fdeaa6603a17a5027cf2dd5da126
Benjamin Laird <benjamin.laird@acmi.net.au>
2019-08-21 14:03:28 +1000

    Create a virtual network interface for Nodel

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
-# Start Nodel
+# Configure virtual network interface for Nodel
+IP_ADDRESS=$(curl -X GET --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v1/device?apikey=$BALENA_SUPERVISOR_API_KEY" | jq .ip_address)
+ip link add nodel0 type dummy
+ifconfig nodel0 $IP_ADDRESS
 mkdir -p /opt/nodel/nodes/$BALENA_DEVICE_NAME_AT_INIT
 cp -f /opt/nodel/scripts/script.py /opt/nodel/nodes/$BALENA_DEVICE_NAME_AT_INIT/
-java -jar /opt/nodel/nodel.jar --interface $NODEL_INTERFACE
+# Start Nodel
+java -jar /opt/nodel/nodel.jar --interface nodel0
