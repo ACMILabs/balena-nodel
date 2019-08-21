43eff8aa6272d0055db6020c5135261dc07b9b93
Benjamin Laird <benjamin.laird@acmi.net.au>
2019-08-21 15:08:30 +1000

    Remove reset

--- script.sh
+++ script.sh
@@ -5,5 +5,4 @@ ifconfig nodel0 $IP_ADDRESS
 mkdir -p /opt/nodel/nodes/$BALENA_DEVICE_NAME_AT_INIT
 cp -f /opt/nodel/scripts/script.py /opt/nodel/nodes/$BALENA_DEVICE_NAME_AT_INIT/
 # Start Nodel
-java -jar /opt/nodel/nodel.jar --interface nodel0 &
-ifconfig nodel0 0.0.0.0
+java -jar /opt/nodel/nodel.jar --interface nodel0
